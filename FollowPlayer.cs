87d4c0341fa8fca53d795d1d43c475cd8ed59411
tscaram <32144935+tscaram@users.noreply.github.com>
2018-08-28 13:25:15 -0400

    Update FollowPlayer.cs

--- FollowPlayer.cs
+++ FollowPlayer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//make the camera follow the player as it moves up.
+
 public class FollowPlayer : MonoBehaviour {
 
     public int offset;
@@ -11,12 +13,12 @@ public class FollowPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        self = GetComponent<Transform>();
+            self = GetComponent<Transform>();
 	}
 	
-	// Update is called once per frame
+	// Once per frame, set the camera equal to the y position of the player, while locking the x and z positions
 	void Update () {
-        self.position = new Vector3(offset, player.transform.position.y, offsetZ);
+            self.position = new Vector3(offset, player.transform.position.y, offsetZ);
 
 
 	}

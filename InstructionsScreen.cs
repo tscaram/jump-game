bf1537f4c20b3bfa92b10d8d30108d40fe3bc6e2
tscaram <32144935+tscaram@users.noreply.github.com>
2018-08-28 13:37:36 -0400

    Update InstructionsScreen.cs

--- InstructionsScreen.cs
+++ InstructionsScreen.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class InstructionsScreen : MonoBehaviour {
-
+    //if you press the main menu button on the screen, it takes you to the main menu
+    
     public void menu()
     {
         SceneManager.LoadScene("MainMenu");

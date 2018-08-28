20e71073077f1bf943ae2559333c511b7d173415
tscaram <32144935+tscaram@users.noreply.github.com>
2018-08-28 13:45:35 -0400

    Update PlayerMove.cs

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+//Allows the player to move left and right
+
 public class PlayerMove : MonoBehaviour {
 
+    //Sets up the variables, including text that tells the player to press p to start and gets the move speed declared in the unity editor
+	
     Rigidbody2D rb;
     public Text startingText;
     public int moveSpeed;
     public static int jumpHeight = 999;
-	// Use this for initialization
-	void Start () {
+	
+    // grabs the rigidbody component of the player and allows force to be added to it, and freezes time
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
         Time.timeScale = 0;
-	}
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P))	//Starts the game when the player presses p. If the game is already going, pause it
         {
             if (Time.timeScale == 0)
             {
@@ -23,18 +30,14 @@ public class PlayerMove : MonoBehaviour {
                 if (startingText.text != null)
                     startingText.text = null;
             }
-
-
             else
             {
                 startingText.text = "Paused. Press P to continue.";
                 Time.timeScale = 0;
-
             }
-
         }
     }
-    private void FixedUpdate()
+    private void FixedUpdate()	//move the player left or right depending on what they press. The controls are specified within the unity editor
     {
         float x = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);

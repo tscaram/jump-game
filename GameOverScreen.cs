023ac967f8f28c784116cd44e524ebe33fcb13a0
tscaram <32144935+tscaram@users.noreply.github.com>
2018-08-28 13:31:53 -0400

    Update GameOverScreen.cs

--- GameOverScreen.cs
+++ GameOverScreen.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class GameOverScreen : MonoBehaviour {
     public Text theEnd;
     
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+	//if you're playing the normal game mode and you win, say you win
         if (ChangeScore.win)
         {
             theEnd.text = "Congratulations! You win!\nPress Esc to start over!";
         }
         else theEnd.text = "Good game! You reached a height of " + ChangeScore.height + " feet.\nPress Esc to start over!";
+	    
+	//if youre playing the endless or hardcore game mode, if you beat the high score, save the high score
         if (PlayerPrefs.HasKey(MainMenuControl.gameMode))
         {
             if (MainMenuControl.gameMode.Equals("Endless"))
@@ -34,13 +38,13 @@ public class GameOverScreen : MonoBehaviour {
             PlayerPrefs.SetInt(MainMenuControl.gameMode, ChangeScore.height);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+    // Go to the main menu when escape is pressed
+    void Update () {
+	if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene("MainMenu");
         }
-	}
+    }
 
 
     public void mainMenu()
